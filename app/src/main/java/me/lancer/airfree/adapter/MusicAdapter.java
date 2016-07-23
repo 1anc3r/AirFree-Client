181e0423e931bf1e8ac54e9b56b2350cec025a51
1anc3r <huangfangzhi@icloud.com>
2016-07-23 20:04:39 +0800

    Add fun. Music Searching and now can show album artwork.

--- app/src/main/java/me/lancer/airfree/adapter/MusicAdapter.java
+++ app/src/main/java/me/lancer/airfree/adapter/MusicAdapter.java
@@ -5,34 +5,50 @@ import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nineoldandroids.animation.AnimatorSet;
 import com.nineoldandroids.animation.ObjectAnimator;
 
+import me.lancer.airfree.model.ImageViewBean;
+import me.lancer.airfree.util.MusicUtil;
 import me.lancer.distance.R;
 import me.lancer.airfree.model.MusicBean;
 
 public class MusicAdapter extends BaseAdapter {
 
     private Context context;
-    private List<MusicBean> mp3List;
     private List<String> posList;
-    private Handler mHandler;
+    private List<String> searchList;
+    private List<MusicBean> musicList;
     protected LayoutInflater mInflater;
+    private Point mPoint = new Point(0, 0);//用来封装ImageView的宽和高的对象
 
-    public MusicAdapter(Context context, List<MusicBean> mp3List, List<String> posList, Handler mHandler) {
+    private Handler mHandler;
+
+    public MusicAdapter(Context context, List<MusicBean> musicList, List<String> posList, List<String> searchList, Handler mHandler) {
         this.context = context;
-        this.mp3List = mp3List;
+        this.musicList = musicList;
+        this.searchList = searchList;
         this.posList = posList;
         this.mHandler = mHandler;
         mInflater = LayoutInflater.from(context);
@@ -40,12 +56,12 @@ public class MusicAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return mp3List.size();
+        return musicList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return mp3List.get(position);
+        return musicList.get(position);
     }
 
 
@@ -57,7 +73,9 @@ public class MusicAdapter extends BaseAdapter {
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         final ViewHolder viewHolder;
-        final String path = mp3List.get(position).getPath();
+        final String path = musicList.get(position).getPath();
+
+        Log.e("IP & PORT", searchList.toString());
 
         if (convertView == null) {
             convertView = mInflater.inflate(R.layout.lv_music_item, null);
@@ -65,10 +83,19 @@ public class MusicAdapter extends BaseAdapter {
             viewHolder.tvArtist0 = (TextView) convertView.findViewById(R.id.tv_artist0);
             viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tv_title);
             viewHolder.mCheckBox = (CheckBox) convertView.findViewById(R.id.cb_music);
+            viewHolder.ivAlbum = (ImageViewBean) convertView.findViewById(R.id.iv_album);
+            viewHolder.ivAlbum.setOnMeasureListener(new ImageViewBean.OnMeasureListener() {
+
+                @Override
+                public void onMeasureSize(int width, int height) {
+                    mPoint.set(width, height);
+                }
+            });
 
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
+            viewHolder.ivAlbum.setImageResource(R.drawable.ic_fm_icon_music);
         }
 
         convertView.setOnClickListener(new View.OnClickListener() {
@@ -82,8 +109,14 @@ public class MusicAdapter extends BaseAdapter {
             }
         });
 
-        viewHolder.tvArtist0.setText(mp3List.get(position).getArtist());
-        viewHolder.tvTitle.setText(mp3List.get(position).getTitle());
+        viewHolder.tvArtist0.setText(musicList.get(position).getArtist());
+        viewHolder.tvTitle.setText(musicList.get(position).getTitle());
+        Bitmap bitmap = MusicUtil.getArtwork(context, musicList.get(position).getId(), musicList.get(position).getAlbumId(), true, true);
+        if (bitmap != null) {
+            viewHolder.ivAlbum.setImageBitmap(bitmap);
+        } else {
+            viewHolder.ivAlbum.setImageResource(R.drawable.ic_fm_icon_music);
+        }
         viewHolder.mCheckBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,12 +129,36 @@ public class MusicAdapter extends BaseAdapter {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                //如果是未选中的CheckBox,则添加动画
                 addAnimation(viewHolder.mCheckBox);
             }
         });
         viewHolder.mCheckBox.setChecked(posList.contains("" + position) ? true : false);
         viewHolder.mCheckBox.bringToFront();
+
+        Log.e("IP & PORT", "adapter" + searchList);
+        String title = musicList.get(position).getTitle();
+        String artist = musicList.get(position).getArtist();
+        if (searchList.size() > 0) {
+            String keyword = searchList.get(0);
+            if ((title != null && title.contains(keyword)) || (artist != null && artist.contains(keyword))) {
+                ForegroundColorSpan span = new ForegroundColorSpan(Color.RED);
+                SpannableStringBuilder builder1 = new SpannableStringBuilder(title);
+                int index1 = title.indexOf(keyword);
+                if (index1 != -1) {
+                    builder1.setSpan(span, index1, index1 + keyword.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                }
+                SpannableStringBuilder builder2 = new SpannableStringBuilder(artist);
+                int index2 = artist.indexOf(keyword);
+                if (index2 != -1) {
+                    builder2.setSpan(span, index2, index2 + keyword.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                }
+                viewHolder.tvTitle.setText(builder1);
+                viewHolder.tvArtist0.setText(builder2);
+            } else {
+                viewHolder.tvTitle.setText(title);
+                viewHolder.tvArtist0.setText(artist);
+            }
+        }
 
         return convertView;
     }
@@ -115,6 +172,7 @@ public class MusicAdapter extends BaseAdapter {
     }
 
     public static class ViewHolder {
+        public ImageViewBean ivAlbum;
         public TextView tvArtist0;
         public TextView tvTitle;
         public CheckBox mCheckBox;
